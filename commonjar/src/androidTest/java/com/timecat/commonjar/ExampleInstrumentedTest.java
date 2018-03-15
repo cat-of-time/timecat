d38661262e9edaee8b2740643b5cde32bac3b36d
兮尘 <761516186@qq.com>
2018-03-15 23:41:05 +0800

    common jar

--- commonjar/src/androidTest/java/com/timecat/commonjar/ExampleInstrumentedTest.java
+++ commonjar/src/androidTest/java/com/timecat/commonjar/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.shang.commonjar;
+package com.timecat.commonjar;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.shang.commonjar.test", appContext.getPackageName());
+        assertEquals("com.timecat.commonjar.test", appContext.getPackageName());
     }
 }
