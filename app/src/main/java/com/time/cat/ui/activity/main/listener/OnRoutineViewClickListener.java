e0ab9d4821974e4cb9d8f4e738e10a68ff5b3309
兮尘 <761516186@qq.com>
2018-03-18 11:55:48 +0800

    update strings en

---
+++ app/src/main/java/com/time/cat/ui/activity/main/listener/OnRoutineViewClickListener.java
@@ -0,0 +1,16 @@
+package com.time.cat.ui.activity.main.listener;
+
+/**
+ * @author dlink
+ * @date 2018/1/25
+ * @discription
+ */
+public interface OnScheduleViewClickListener {
+    void onViewTodayClick();
+
+    void onViewRefreshClick();
+
+    void onViewChangeMarkThemeClick();
+
+    void onViewExpand();
+}
