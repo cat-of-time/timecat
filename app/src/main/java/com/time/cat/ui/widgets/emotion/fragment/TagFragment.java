c75a3a29ceb542cc40b48e0aff4f08b3f6cf6944
兮尘 <761516186@qq.com>
2018-03-27 00:00:21 +0800

    tag fgm

--- app/src/main/java/com/time/cat/ui/widgets/emotion/fragment/TagFragment.java
+++ app/src/main/java/com/time/cat/ui/widgets/emotion/fragment/TagFragment.java
@@ -4,10 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.time.cat.R;
+import com.time.cat.data.database.DB;
+import com.time.cat.data.model.APImodel.Tag;
+import com.time.cat.data.model.Converter;
 import com.time.cat.ui.base.BaseFragment;
+import com.time.cat.ui.widgets.label_tag_view.TagCloudView;
+import com.time.cat.util.override.LogUtil;
+import com.time.cat.util.string.MeechaoDataUtils;
+import com.time.cat.util.string.TimeUtil;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author dlink
@@ -16,13 +26,56 @@ import com.time.cat.ui.base.BaseFragment;
  * @discription null
  * @usage null
  */
-public class Fragment1 extends BaseFragment {
+public class TagFragment extends BaseFragment {
+    TagCloudView tagCloudView;
+
+
+    /**
+     * 创建与Fragment对象关联的View视图时调用
+     * @param inflater inflater
+     * @param container container
+     * @param savedInstanceState savedInstanceState
+     * @return view
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView= LayoutInflater.from(getActivity()).inflate(R.layout.fragment1,null);
-        TextView tv= (TextView) rootView.findViewById(R.id.tv);
-
-        tv.setText(args.getString("Interge"));
-        return rootView ;
+        View rootView = inflater.inflate(R.layout.fragment_tag, container, false);
+        tagCloudView = rootView.findViewById(R.id.tag_cloud_view);
+        List<Tag> tagList = new ArrayList<>();
+        for (int i = 1 ; i <= 40; i++) {
+            Tag tag = new Tag();
+            tag.setOwner(Converter.getOwnerUrl(DB.users().getActive()));
+            tag.setCreated_datetime(TimeUtil.formatGMTDate(new Date()));
+            tag.setName(" #标签"+i+"["+i+"|话题]# ");
+            tag.setUrl("http://192.168.88.105:8000/tags/"+i+"/");
+            tagList.add(tag);
+        }
+        List<String> tags = new ArrayList<>();
+        for (Tag t: tagList) {
+            tags.add(MeechaoDataUtils.regexTag(t.getName()));
+        }
+        tagCloudView.setTags(tags);
+        tagCloudView.setOnTagClickListener(position -> {
+            if (position == -1) {
+                LogUtil.e("onClick tagCloudView at --> 点击末尾文字");
+            } else {
+                LogUtil.e("onClick tagCloudView at --> 点击 position" + position);
+                if (onTagAddListener != null) {
+                    onTagAddListener.addTag(tagList.get(position));
+                }
+            }
+        });
+        return rootView;
     }
+
+    OnTagAddListener onTagAddListener;
+
+    public void setOnTagAddListener(OnTagAddListener onTagAddListener) {
+        this.onTagAddListener = onTagAddListener;
+    }
+
+    public interface OnTagAddListener {
+        void addTag(Tag tag);
+    }
+
 }
