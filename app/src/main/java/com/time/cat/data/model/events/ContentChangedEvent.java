b2d89fa1fab94ac4605452465cd4d7969ed8f3c0
761516186@qq.com <761516186@qq.com>
2018-04-12 23:34:14 +0800

    remove data/*

--- app/src/main/java/com/time/cat/data/model/events/ContentChangedEvent.java
+++
@@ -1,9 +0,0 @@
-package com.time.cat.data.model.events;
-
-public class ContentChangedEvent {
-    public String content;
-
-    public ContentChangedEvent(String content) {
-        this.content = content;
-    }
-}
